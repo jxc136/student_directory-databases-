55f371e65ffb5e93aeda16dbca1c0169abec8c9e
Josh Carvell <j_carvell@hotmail.co.uk>
2022-11-25 17:04:06 +0000

    added app.rb demonstration

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'lib/database_connection'
+require_relative 'lib/cohort_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('student_directory_two')
+
+repo = CohortRepository.new 
+
+cohort = repo.find_with_students(3)
+
+p cohort.name 
+cohort.students.each { |student| p student.name }
